12b2c359bdee54d276ebd7105dcc32e5444b2f9f
Kaydax <kaydax@kaydax.xyz>
2023-07-16 23:42:43 -0400

    JumpList crash fix Rename Assembly Make gitignore the same as the template Add Publishing file

--- Library.cs
+++ Library.cs
@@ -7,18 +7,19 @@ public class Patch //The class name must also remain Patch
   //This is our entry point into our patch! This is where we create the harmony instance and do all our cool patch stuff
   public static void DoPatching()
   {
-    var harmony = new Harmony("com.example.patch");
+    var harmony = new Harmony("xyz.kaydax.toolspatches");
     harmony.PatchAll();
   }
 
   //An example patch which patches Sandbox.Engine.Sandbox.JumpListManager's Startup Method. We then prefix a console log message at the top of the method
-  [HarmonyPatch("JumpListManager", "Startup")]
-  class ExamplePatch
+  [HarmonyPatch("JumpListManager", "BuildJumpList")]
+  class JumpListPatch
   {
     [HarmonyPrefix]
-    static void Prefix()
+    static bool Prefix()
     {
-      Console.WriteLine("Hello from external example patch!!!");
+      Console.WriteLine("[Tools Patch] Jump List currently are broken under proton, so lets patch that out!");
+      return false;
     }
   }
 }
